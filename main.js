43fb71fb22651533281b396652a3b3a82f9ce335
RupanSamanta <rvsjan32@gmail.com>
2025-10-04 18:56:24 +0530

    build desktop app

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
-const createWindow = () => {
+function createWindow() {
   const win = new BrowserWindow({
-    width: 500,
-    height: 750,
-    autoHideMenuBar: true, 
-    icon: path.join(__dirname, "assets/images/icon.ico")
-  })
+    width: 450,
+    height: 620,
+    icon: path.join(__dirname, 'build', 'icon.ico'), // custom icon
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,        // keep false for security
+      contextIsolation: true,        // keep true for security
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
 
-  win.loadFile('index.html')
+  Menu.setApplicationMenu(null); // completely remove default menu
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.whenReady().then(createWindow);
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  if (process.platform !== 'darwin') app.quit();
+});
